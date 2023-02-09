307a4699c065d1954bd92eb4f47a92ba35639c90
keithkahura <kamaukeith391@gmail.com>
2023-02-09 10:50:43 +0300

    lesson 3

---
+++ area_of_a_circle.py
@@ -0,0 +1,6 @@
+#this program calculates the area of a circle
+#the area of a circle
+pi = 3.14
+radius = 24
+area = pi * radius * radius
+print ( area)
---
+++ conditions.py
@@ -0,0 +1,10 @@
+#program to record temperture
+num = eval (input ("enter num"))
+if num <0 :
+    print("wrong input")
+else :
+    if num > 50 :
+         x = num/2
+         print (x)
+    else :
+        print ("number is less than 50")
